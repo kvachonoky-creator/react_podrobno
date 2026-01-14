5472acf368e1ea2bebd6cac97b1798e8e42cdbc7
ya_yarek <kvachonoky@gmail.com>
2026-01-15 00:05:56 +0300

    15_01

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { ControlledOnOff } from './ControlledOnOff';
-import { ItemType, Select } from './Select';
+import {ItemType, Select} from "./Select";
 
 function App() {
 
-  const [onOff, setOnOff] = useState<boolean>(false)
+const [onOff, setOnOff] = useState<boolean>(false)
 
-  const initialState = [
-    { title: "Minsk", value: 1 },
-    { title: "Praga", value: 2 },
-    { title: "Vena", value: 3 },
-  ]
+    const initialState = [
+        {title: "", value: 0},
+        { title: "Minsk", value: 1 },
+        { title: "Praga", value: 2 },
+        { title: "Vena", value: 3 },
+    ]
 
-  const [items, setItems] = useState < ItemType[]> ([])
+    const [items, setItems] = useState < ItemType[]> ([])
 
-  const onChangeSelect = (value: any) => {
-    const newValue = initialState.find(v => v.value === value)
-    setItems(newValue? [newValue] : [])
-  }
+    const onChangeSelect = (value: any) => {
+        const newValue = initialState.find(v => v.value === value)
+        setItems(newValue? [newValue] : [])
+    }
 
   return (
     <div className={"App"}>
-      {/* <ControlledOnOff
-        on={onOff}
-        changeOnOFf={(value) => setOnOff(value)}
-      /> */}
-      <Select items={initialState} onChange={onChangeSelect} value={items} />
+      {/*<ControlledOnOff*/}
+      {/* on={onOff}*/}
+      {/* changeOnOFf={(value) => setOnOff(value)}*/}
+      {/*  />*/}
+        <Select items={initialState} value={items} onChange={onChangeSelect}></Select>
     </div>
   );
 }
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,5 +1,4 @@
-import { log } from "console"
-import { useState } from "react"
+import {useEffect, useRef, useState} from "react"
 
 export type ItemType = {
     title: string
@@ -16,21 +15,46 @@ export function Select(props: SelectPropsType) {
 
     const [option, setOption] = useState<boolean>(false)
 
-console.log(props.value)
+    const onBlurHandler = () => {
+        setOption(false)
+    }
+
+    const onClickHandler = (value: any) => {
+        props.onChange(value)
+        onBlurHandler()
+    }
+
+    const onClickHandlerFocus = () => {
+        setOption(true)
+    }
+
 
     return (
 
         <div>
-            <div >
+            <div
+                 tabIndex={-1} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 {option
-                    ? props.items.map(i => <div autoFocus tabIndex={0} onBlur={() => setOption(false)} onClick={() => props.onChange(i.value)} key={i.value}>{i.title}</div>)
+                    ? props.items.map((i, index) => <div
+                        style={{
+                            backgroundColor: "#cae4db",
+                            width: "100px",
+                            height: "20px",
+                            border: "1px solid #ccc",
+                        }}
+                        onBlur={onBlurHandler}
+                        onClick={() => onClickHandler(i.value)}
+                        key={index}>{i.title}</div>)
                     : <div style={{
+                        position: "relative",
+                        display: "flex",
                         width: "100px",
-                        backgroundColor: "#013f2a",
+                        backgroundColor: "#cae4db",
                         height: "20px"
                     }}
-                        onClick={() => setOption(true)}
-                    >{props.value}</div>
+                           onClick={onClickHandlerFocus}
+                    >{props.value[0]?.title} <span style={{position: "absolute", content: "", right: "0"}}>↴</span>
+                    </div>
                 }
             </div>
 
